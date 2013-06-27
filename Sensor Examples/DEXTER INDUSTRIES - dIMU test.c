605ddac111d8c6d624898c1b7ec04d4129c95f16
Jaikrishna <t.s.jaikrishna@gmail.com>
2013-06-27 16:40:41 +0530

    The values are parsed properly and the Program works well now

--- Sensor Examples/DEXTER INDUSTRIES - dIMU test.c
+++ Sensor Examples/DEXTER INDUSTRIES - dIMU test.c
@@ -1,11 +1,13 @@
+
 /*
 *  Jaikrishna
 *  t.s.jaikrishna<at>gmail.com
-*  Initial date: June 21, 2013
+*  Initial date: June 27, 2013
 *  Based on Matthew Richardson's example on testing BrickPi drivers.
 *  You may use this code as you wish, provided you give credit where it's due.
 *  
 *  This is a program for testing the RPi BrickPi drivers and I2C communication on the BrickPi with a dIMU
+*  The program continuously polls values from the dIMU and displays Accelerometer & Gyroscope Readings
 */
 
 #include <stdio.h>
@@ -32,15 +34,16 @@
 
 int result;
 
-
-float           X, Y, Z;
+int xx,yy,zz;
+short int           X, Y, Z;
+float x,y,z;
 float angle;
 
 #define PI 3.14159265359
-#define I2C_PORT  PORT_1                             // I2C port for the dIMU
-#define I2C_SPEED 0                                  // delay for as little time as possible. Usually about 100k baud
+#define I2C_PORT  PORT_1              // I2C port for the dIMU
+#define I2C_SPEED 0                   // delay for as little time as possible. Usually about 100k baud
 
-#define I2C_DEVICE_DCOM 0                        // DComm is device 0 on this I2C bus
+#define I2C_DEVICE_DIMU 0             // DComm is device 0 on this I2C bus
 
 #define DIMU_GYRO_I2C_ADDR      0xD2  /*!< Gyro I2C address */
 
@@ -90,105 +93,152 @@ int main() {
 
   BrickPi.SensorI2CDevices [I2C_PORT]    = 1;
   
-/* Pseudo code for reading Gyro data:
-Configuration:
-SEND Addr: DIMU_GYRO_I2C_ADDR
-1  REG2, 0x00
-2  REG3, 0x08
-3  REG4, range+BLOCKDATA
-4  REG5, 0x02
-5  REG1, 0x0F
+  /* Pseudo code for reading Gyro data:
+  Configuration:
+  SEND Addr: DIMU_GYRO_I2C_ADDR
+  1  REG2, 0x00
+  2  REG3, 0x08
+  3  REG4, range+BLOCKDATA
+  4  REG5, 0x02
+  5  REG1, 0x0F
+  
+  SET Gyro_div=57.142857 //for range 500 dps
+  
+  Reading measurements:
+  SEND Addr DIMU_GYRO_I2C_ADDR
+  DIMU_GYRO_ALL_AXES+0x80  -> READ 6 BYTES
+  
+  Y = ([0]+((long)([1]<<8)))/Gyro_div
+  X = ([2]+((long)([3]<<8)))/Gyro_div
+  Z = ([4]+((long)([5]<<8)))/Gyro_div
+  */
 
-SET Gyro_div=57.142857 //for range 500 dps
 
-Reading measurements:
-SEND Addr DIMU_GYRO_I2C_ADDR
-DIMU_GYRO_ALL_AXES+0x80  -> READ 6 BYTES
-
-Y = ([0]+((long)([1]<<8)))/Gyro_div
-X = ([2]+((long)([3]<<8)))/Gyro_div
-Z = ([4]+((long)([5]<<8)))/Gyro_div
-*/
-
-
-  BrickPi.SensorSettings   [I2C_PORT][I2C_DEVICE_DCOM] = 0;  
-  BrickPi.SensorI2CAddr    [I2C_PORT][I2C_DEVICE_DCOM] = DIMU_GYRO_I2C_ADDR;	//address for writing
+  BrickPi.SensorSettings   [I2C_PORT][I2C_DEVICE_DIMU] = 0;  
+  BrickPi.SensorI2CAddr    [I2C_PORT][I2C_DEVICE_DIMU] = DIMU_GYRO_I2C_ADDR;	//address for writing
   
   if(BrickPiSetupSensors())
     return 0;
 	
   //1
-  BrickPi.SensorI2CWrite [I2C_PORT][I2C_DEVICE_DCOM]    = 2;	//number of bytes to write
-  BrickPi.SensorI2CRead  [I2C_PORT][I2C_DEVICE_DCOM]    = 0;	//number of bytes to read
+  BrickPi.SensorI2CWrite [I2C_PORT][I2C_DEVICE_DIMU]    = 2;	//number of bytes to write
+  BrickPi.SensorI2CRead  [I2C_PORT][I2C_DEVICE_DIMU]    = 0;	//number of bytes to read
   
-  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DCOM][0] = DIMU_GYRO_CTRL_REG2;	//byte to write
-  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DCOM][1] = 0x00;	//byte to write
+  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][0] = DIMU_GYRO_CTRL_REG2;	//byte to write
+  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][1] = 0x00;	//byte to write
   if(BrickPiUpdateValues())		//writing
     return 0;
-  if(!(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_DCOM)))	//BrickPi.Sensor[PORT] has an 8 bit number consisting of success(1) or failure(0) on all ports in bus
+  if(!(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_DIMU)))	//BrickPi.Sensor[PORT] has an 8 bit number consisting of success(1) or failure(0) on all ports in bus
     return 0;
   
   //2
   //we're writing 2 bytes again, so there's no need to redefine number of butes
-  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DCOM][0] = DIMU_GYRO_CTRL_REG3;
-  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DCOM][1] = 0x08;  
+  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][0] = DIMU_GYRO_CTRL_REG3;
+  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][1] = 0x08;  
   if(BrickPiUpdateValues())
     return 0;
-  if(!(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_DCOM)))
+  if(!(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_DIMU)))
     return 0;  
   //3
-  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DCOM][0] = DIMU_GYRO_CTRL_REG4;
-  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DCOM][1] = DIMU_GYRO_RANGE_500+DIMU_CTRL4_BLOCKDATA;  
+  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][0] = DIMU_GYRO_CTRL_REG4;
+  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][1] = DIMU_GYRO_RANGE_500+DIMU_CTRL4_BLOCKDATA;  
   if(BrickPiUpdateValues())
     return 0;
-  if(!(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_DCOM)))
+  if(!(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_DIMU)))
     return 0;  
   //4
-  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DCOM][0] = DIMU_GYRO_CTRL_REG5;
-  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DCOM][1] = 0x02;  
+  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][0] = DIMU_GYRO_CTRL_REG5;
+  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][1] = 0x02;  
   if(BrickPiUpdateValues())
     return 0;
-  if(!(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_DCOM)))
+  if(!(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_DIMU)))
     return 0;  
   //5
-  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DCOM][0] = DIMU_GYRO_CTRL_REG1;
-  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DCOM][1] = 0x0F;  
+  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][0] = DIMU_GYRO_CTRL_REG1;
+  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][1] = 0x0F;  
   if(BrickPiUpdateValues())
     return 0;
-  if(!(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_DCOM)))
+  if(!(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_DIMU)))
     return 0;  
   
   float Gyro_div=57.142857; 
   
-  usleep(100000);
+  //Initializing Accelerometer to 2G range
+  BrickPi.SensorI2CAddr    [I2C_PORT][I2C_DEVICE_DIMU] = DIMU_ACC_I2C_ADDR;
+  BrickPi.SensorI2CWrite [I2C_PORT][I2C_DEVICE_DIMU]    = 2;
+  BrickPi.SensorI2CRead  [I2C_PORT][I2C_DEVICE_DIMU]    = 0;  
+  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][0] = 0x16;
+  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][1] = DIMU_ACC_RANGE_2G | DIMU_ACC_MODE_MEAS;
+  result = BrickPiSetupSensors();
+  BrickPiUpdateValues();
+
+  usleep(10000);
+  
   //loop
+  //first reading the x,y,z of Accelerometer in 3 steps. 
   //writing DIMU_GYRO_ALL_AXES + 0x08 at gyro address 
   //and then reading 6 bytes 
-  BrickPi.SensorSettings [I2C_PORT][I2C_DEVICE_DCOM]    = BIT_I2C_SAME;
-  BrickPi.SensorI2CWrite [I2C_PORT][I2C_DEVICE_DCOM]    = 1;
-  BrickPi.SensorI2CRead  [I2C_PORT][I2C_DEVICE_DCOM]    = 6;  
-  BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DCOM][0] = DIMU_GYRO_ALL_AXES + 0x80;
-  result = BrickPiSetupSensors();
+ 
   printf("BrickPiSetupSensors: %d\n", result); 
   if(!result){
-    
-    usleep(100000);
-    
     while(1){
-      result = BrickPiUpdateValues();
-      if(!result){
+      //ACC
+      BrickPi.SensorI2CAddr    [I2C_PORT][I2C_DEVICE_DIMU] = DIMU_ACC_I2C_ADDR;
+      BrickPi.SensorI2CWrite [I2C_PORT][I2C_DEVICE_DIMU]    = 1;
+      BrickPi.SensorI2CRead  [I2C_PORT][I2C_DEVICE_DIMU]    = 1;  
+      BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][0] = 0x06;
+      BrickPiSetupSensors();
+      BrickPiUpdateValues();
+      xx = BrickPi.SensorI2CIn   [I2C_PORT][I2C_DEVICE_DIMU][0];
+      if(xx>128) xx-=256;
+      x = ((float)(xx))/64.0;
+      
+      BrickPi.SensorI2CWrite [I2C_PORT][I2C_DEVICE_DIMU]    = 1;
+      BrickPi.SensorI2CRead  [I2C_PORT][I2C_DEVICE_DIMU]    = 1;  
+      BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][0] = 0x07;
+      BrickPiSetupSensors();
+      BrickPiUpdateValues();
+      yy = BrickPi.SensorI2CIn   [I2C_PORT][I2C_DEVICE_DIMU][0];
+      if(yy>128) yy-=256;
+      y = ((float)(yy))/64.0;
+      
+      BrickPi.SensorI2CWrite [I2C_PORT][I2C_DEVICE_DIMU]    = 1;
+      BrickPi.SensorI2CRead  [I2C_PORT][I2C_DEVICE_DIMU]    = 1;  
+      BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][0] = 0x08;
+      BrickPiSetupSensors();
+      BrickPiUpdateValues();
+      zz = BrickPi.SensorI2CIn   [I2C_PORT][I2C_DEVICE_DIMU][0];
+      if(zz>128) zz-=256;
+      z = ((float)(zz))/64.0;
+      
+      printf("x:%f y:%f z:%f",x,y,z);
+      
+      //GYRO
+      BrickPi.SensorI2CAddr    [I2C_PORT][I2C_DEVICE_DIMU] = DIMU_GYRO_I2C_ADDR;
+      BrickPi.SensorSettings [I2C_PORT][I2C_DEVICE_DIMU]    = BIT_I2C_SAME;
+      BrickPi.SensorI2CWrite [I2C_PORT][I2C_DEVICE_DIMU]    = 1;
+      BrickPi.SensorI2CRead  [I2C_PORT][I2C_DEVICE_DIMU]    = 6;  
+      BrickPi.SensorI2COut   [I2C_PORT][I2C_DEVICE_DIMU][0] = DIMU_GYRO_ALL_AXES + 0x80;
+      result = BrickPiSetupSensors();
+      BrickPiUpdateValues();
 
-        if(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_DCOM)){
-          Y = (BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_DCOM][0]+(long)((BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_DCOM][1])<<8))/Gyro_div;
-          X = (BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_DCOM][2]+(long)((BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_DCOM][3])<<8))/Gyro_div;
-          Z = (BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_DCOM][4]+(long)((BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_DCOM][5])<<8))/Gyro_div;
-          
-         
-          printf("X: %d  Y: %d  Z: %d  \n", X, Y, Z);
-        
+        if(BrickPi.Sensor[I2C_PORT] & (0x01 << I2C_DEVICE_DIMU)){
+	  Y = (BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_DIMU][0]|(BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_DIMU][1]<<8));
+          X = (BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_DIMU][2]|(BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_DIMU][3]<<8));
+          Z = (BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_DIMU][4]|(BrickPi.SensorI2CIn[I2C_PORT][I2C_DEVICE_DIMU][5]<<8));
+	  
+	  if(X > 32767) X = -1*(65536-X);
+	  x=(float)(X/Gyro_div);
+	  if(Y > 32767) Y = -1*(65536-Y);
+	  y=(float)(Y/Gyro_div);
+	  if(Z > 32767) Z = -1*(65536-Z);
+	  z=(float)(Z/Gyro_div);
+	  
+          printf(" X:%f Y:%f Z:%f\n",x,y,z);
+
         }
-      }
-      usleep(100000);
+      
+      usleep(1000000);
     }
   }
   return 0;
