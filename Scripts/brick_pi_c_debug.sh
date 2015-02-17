e5b392e849a4df35eab0b3edba3680b7947087e2
Dexter Industries <dxtrindustries@gmail.com>
2015-02-17 16:16:28 -0500

    Remove wiringPi
    REmoved wiringPi installation.

--- Scripts/brick_pi_c_debug.sh
+++ Scripts/brick_pi_c_debug.sh
@@ -2,7 +2,6 @@
 sudo apt-get update
 cd /tmp/
 wget https://raw.githubusercontent.com/DexterInd/BrickPi/master/Setup%20Files/install.sh
-wget https://github.com/DexterInd/BrickPi/raw/master/Setup%20Files/wiringPi.zip 
 chmod +x install.sh
 ./install.sh
 
